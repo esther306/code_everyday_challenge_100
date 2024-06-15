8038494d49e4072aff2a8a963834a5fd43fea7db
kengne esther provisaline <estherprovisalinekengne@gmail.com>
2024-06-15 22:07:43 +0100

    My tutorial for day 6

--- DAY_6/anim.js
+++ DAY_6/anim.js
@@ -1,25 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const boxes = document.querySelectorAll('.box');
-    let triggerPoint = window.innerHeight * 0.8;
+const boxes = document.querySelectorAll('.box')
 
-    function handleScroll() {
-        boxes.forEach((box, index) => {
-            const boxTop = box.getBoundingClientRect().top;
-            if (boxTop < triggerPoint) {
-                if (index % 2 === 0) {
-                    box.classList.add('show-from-left');
-                    box.classList.remove('content');
-                } else {
-                    box.classList.add('show-from-right');
-                    box.classList.remove('content');
-                }
-            } else {
-                box.classList.add('content');
-                box.classList.remove('show-from-left', 'show-from-right');
-            }
-        });
-    }
+console.log(boxes)
+window.addEventListener('scroll', checkBoxes)
 
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
-});
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 6 * 4
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    })
+}
